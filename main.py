10aa886f4e3057932dcc8a9f5c5fa48517e499ce
samuel23taku <tsmakadho@gmail.com>
2024-07-16 17:52:15 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import torch
+from torch.utils.data import dataset
+from torchvision import datasets,transforms
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5),(0.5))
+    ])
+
+training_set = datasets.FashionMNIST(
+        root="./data",
+        train=True,
+        download=True,
+        transform=transform
+        )
+test_set = datasets.FashionMNIST(
+        root="./data",
+        train=False,
+        download=True,
+        transform=transform
+        )
+image,label = training_set[0]
+
+train_loader = torch.utils.data.DataLoader(
+        dataset=training_set,
+        batch_size = 64,
+        shuffle=True
+        )
+test_loader = torch.utils.data.DataLoader(
+        dataset=test_set,
+        batch_size=64,
+        shuffle=False
+        )
+
+EPOCH_COUNT = 8
+
+for item in range(0,EPOCH_COUNT):
+    print(item)
+print(image.shape)
+
