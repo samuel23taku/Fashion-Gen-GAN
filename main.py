35366446efc0d92d5449376fb5ce0997eb226510
samuel23taku <tsmakadho@gmail.com>
2024-07-17 20:55:20 +0200

    Discriminator.py

--- main.py
+++ main.py
@@ -1,45 +1,47 @@
 import pandas as pd
 import torch
 from torch.utils.data import dataset
-from torchvision import datasets,transforms
+from torchvision import datasets, transforms
 import matplotlib
 import torch.nn as nn
+import torch.optim as optim
+from models.discriminator import Discriminator
 from models.generator import Generator
-import  utils.constants as Constants
+import utils.constants as Constants
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
-
 transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.5),(0.5))
-    ])
+    transforms.Normalize((0.5), (0.5))
+])
 
 training_set = datasets.FashionMNIST(
-        root="./data",
-        train=True,
-        download=True,
-        transform=transform
-        )
+    root="./data",
+    train=True,
+    download=True,
+    transform=transform
+)
 test_set = datasets.FashionMNIST(
-        root="./data",
-        train=False,
-        download=True,
-        transform=transform
-        )
-image,label = training_set[0]
+    root="./data",
+    train=False,
+    download=True,
+    transform=transform
+)
+image, label = training_set[0]
 
 train_loader = torch.utils.data.DataLoader(
-        dataset=training_set,
-        batch_size = 64,
-        shuffle=True
-        )
+    dataset=training_set,
+    batch_size=64,
+    shuffle=True
+)
 test_loader = torch.utils.data.DataLoader(
-        dataset=test_set,
-        batch_size=64,
-        shuffle=False
-        )
+    dataset=test_set,
+    batch_size=64,
+    shuffle=False
+)
+
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -49,11 +51,22 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
-generator = Generator(Constants.INPUT_SIZE_VECTOR,Constants.NGF,Constants.NC)
+
+fixed_noise = torch.randn(64, Constants.NOISE_VECTOR_SIZE, 1, 1)
+
+generator = Generator(Constants.NOISE_VECTOR_SIZE, Constants.NGF, Constants.NC)
+discriminator = Discriminator(Constants.NC, Constants.NGF)
+
+optimizerD = optim.Adam(discriminator.parameters(), lr=Constants.LEARNING_RATE)
+
+'''
+Weight initialization
+'''
 generator.apply(weights_init)
-print(generator)
+discriminator.apply(weights_init)
+
+criterion = nn.BCELoss()  # for binary cross entropy for the discriminator
 
 # for item in range(0,EPOCH_COUNT):
 #     print(item)
 # print(image.shape)
-
--- models/discriminator.py
+++ models/discriminator.py
@@ -1,8 +1,20 @@
 import torch.nn as nn
 
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-        self.main = nn.Sequential(
 
-        )
+class Discriminator(nn.Module):
+    def __init__(self, nc, ndf):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(nn.Conv2d(nc, ndf, kernel_size=3, stride=1, padding=1, bias=False),
+                                  nn.LeakyReLU(0.2, inplace=True),
+                                  nn.Conv2d(ndf, ndf * 2, kernel_size=3, stride=1, padding=1, bias=False),
+                                  nn.BatchNorm2d(ndf * 2), nn.LeakyReLU(0.2, inplace=True),
+                                  nn.Conv2d(ndf * 2, ndf * 4, kernel_size=3, stride=1, padding=1, bias=False),
+                                  nn.BatchNorm2d(ndf * 4), nn.LeakyReLU(0.2, inplace=True),
+                                  nn.Conv2d(ndf * 4, ndf * 8, kernel_size=3, stride=1, padding=1, bias=False),
+                                  nn.BatchNorm2d(ndf * 4), nn.LeakyReLU(0.2, inplace=True),
+                                  nn.Conv2d(ndf * 8, 1, kernel_size=3, stride=1, padding=0, bias=False),
+                                  nn.Sigmoid()
+                                  )
+
+    def forward(self, x):
+        return self.main(x)
